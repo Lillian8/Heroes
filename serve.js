576b2c39013e1a26c86af283a64b3c45a23ba9da
Lillian8 <teamo.xinitha@gmail.com>
2018-12-12 14:30:46 -0500

    modificaciones para servidor

--- serve.js
+++ serve.js
@@ -1,11 +1,19 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path');
+const app = express()
 
 
 app.use(express.static(__dirname + '/dist/spa'))
+app.get('*', function(req, res) {
+    // res.sendfile('./dist/spa/index.html')
+    res.sendFile(path.join(__dirname + '/dist/spa/index.html'));
+})
 
-// app.get('/', function(req, res) {
-//     res.send('Hello World')
-// })
+// app.get('/*', function(req,res) {
 
-app.listen(3000)
+//     res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+//     });
+
+app.listen(3000, () => {
+    console.log("corriendo en el puerto 3000");
+})
